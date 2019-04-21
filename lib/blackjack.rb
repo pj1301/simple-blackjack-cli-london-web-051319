70a1b3f0edf389a90d111be3345353181127f2d6
James Hayward <james.hayward6@icloud.com>
2019-04-21 18:13:28 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def welcome
   # code #welcome here
   puts "Welcome to the Blackjack Table"
@@ -5,45 +7,58 @@ end
 
 def deal_card
   # code #deal_card here
-# card = rand(1..11)
-  11.times do
-    puts rand(1..11)
+    return rand(1..11)
   end
-end
+# end
 
 def display_card_total(card_total)
   # code #display_card_total here
   puts "Your cards add up to #{card_total}"
+  return card_total
 end
 
 def prompt_user
   # code #prompt_user here
-  print "Will you hit, or will you stay?"
-  hit_or_stay = gets.chomp
-  if hit_or_stay === "h"
-    return "hit"
-  elsif hit_or_stay === "s"
-    return "stay"
-  else
-    return nil
-  end
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  hit_or_stay = gets.chomp
+  if hit_or_stay === "h"
+    return "h"
+  elsif hit_or_stay === "s"
+    return "s"
+  else
+    return "exit"
+  end
 end
 
-def end_game
+def end_game(num)
   # code #end_game here
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+# code #initial_round here
+  sum = deal_card + deal_card # this will call the prior function
+#  binding.pry used PRY to try to work out an initial error
+  display_card_total(sum)
+  return sum
 end
 
-def hit?
+def hit?(value)
   # code hit? here
+ prompt_user
+ get_user_input
+  while get_user_input == "h"
+    deal_card
+    display_card_total(value)
+  
+  end
+ display_card_total(value)
 end
+
 
 def invalid_command
   # code invalid_command here
